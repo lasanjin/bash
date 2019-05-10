d2f40036cd26caf948888c4d2c74465540f0a43e
anthager <anthage@student.chalmers.se>
2019-05-10 12:05:35 +0200

    edit the path where the script is downloaded to ~/ instead of ~/Downloads

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 install_express () {
 	export FILE="express"
-	curl "https://raw.githubusercontent.com/lasanjin/expressen-lunch-cli/master/expressen.py" > ~/Downloads/$FILE
-	chmod +x ~/Downloads/$FILE
-	mv ~/Downloads/$FILE /usr/local/bin
+	curl "https://raw.githubusercontent.com/lasanjin/expressen-lunch-cli/master/expressen.py" > ~/$FILE
+	chmod +x ~/$FILE
+	mv ~/$FILE /usr/local/bin
 }
 install_express
