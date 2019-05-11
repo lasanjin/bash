50a4b6a68938cccf44df97ff49591e059941b2d1
lasanjin <sanjinslavnic@gmail.com>
2019-05-11 15:35:02 +0200

    Ads 'sudo mv'

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ install_express () {
 	export FILE="express"
 	curl "https://raw.githubusercontent.com/lasanjin/expressen-lunch-cli/master/expressen.py" > ~/$FILE
 	chmod +x ~/$FILE
-	mv ~/$FILE /usr/local/bin
+	sudo mv ~/$FILE /usr/local/bin
 }
 install_express
