672f010dee94656b3a2be9d6f42661e1144c157f
lasanjin <sanjinslavnic@gmail.com>
2019-05-12 16:08:05 +0200

    Updates file names + README + install file

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-install_express () {
-	export FILE="express"
-	curl "https://raw.githubusercontent.com/lasanjin/expressen-lunch-cli/master/expressen.py" > ~/$FILE
+install_clunch () {
+	export FILE="clunch"
+	curl "https://raw.githubusercontent.com/lasanjin/chalmers-lunch-cli/master/clunch.py" > ~/$FILE
 	chmod +x ~/$FILE
 	sudo mv ~/$FILE /usr/local/bin
 }
-install_express
+install_clunch
