7c16306c4f768eab86a6c30724a1d776584cd6cb
lasanjin <sanjinslavnic@gmail.com>
2019-05-13 08:47:34 +0200

    Fixes Pripps menu xml parsing + formats code + updates install file and README

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-install_clunch () {
+
+install_clunch() {
 	export FILE="clunch"
-	curl "https://raw.githubusercontent.com/lasanjin/chalmers-lunch-cli/master/clunch.py" > ~/$FILE
+	curl "https://raw.githubusercontent.com/lasanjin/chalmers-lunch-cli/master/clunch.py" >~/$FILE
 	chmod +x ~/$FILE
 	sudo mv ~/$FILE /usr/local/bin
 }
+
 install_clunch
